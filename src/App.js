5a7b36314c8df5687596b25326fe9601902a2143
Fanny <fannette.6576@gmail.com>
2019-10-21 16:17:31 +0200

    cleaning project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-     <Contact name = 'Anita Sutton' avatar = "https://randomuser.me/api/portraits/women/57.jpg" online />
-     <Contact name = ' Jim George' avatar = "https://randomuser.me/api/portraits/men/11.jpg"  />
-     <Contact name = 'Charlotte Ryan' avatar ="https://randomuser.me/api/portraits/women/68.jpg" online/>
+      <Contact name='Anita Sutton' avatar="https://randomuser.me/api/portraits/women/57.jpg" online />
+      <Contact name=' Jim George' avatar="https://randomuser.me/api/portraits/men/11.jpg" />
+      <Contact name='Charlotte Ryan' avatar="https://randomuser.me/api/portraits/women/68.jpg" online />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import './Contact.css';
 
-const Contact = ({name , avatar , online}) => {
-    return (
-         <div className="Contact">
-            <img className="avatar" src={avatar} alt="" />
-            <h1>{name}</h1>
-            <div className="status">
-                <div className={online ? 'status-online' : 'status-offline'} ></div>
-                {
-                    online ? 'Online' : 'Offline'   
-                }
-            </div>
-        </div>
-    )
-}  
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOnline: false,
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-export default Contact
+
+    handleClick() {
+        this.setState({isOnline : !this.state.isOnline })
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt="" />
+                <h1>{this.props.name}</h1>
+                <div className="status" onClick={this.handleClick}>
+                    <div className={this.state.isOnline ? 'status-online' : 'status-offline'}></div>
+                    {this.state.isOnline ? 'Online' : 'Offline'}
+                </div>
+            </div>
+        )
+    }
+
+}
+export default Contact;
